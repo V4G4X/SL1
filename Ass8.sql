78f78f991b28a330fb3bb71299938fdccbd2a79d
V4G4X <varun.gawande@gmail.com>
2019-08-14 15:59:50 +0530

    Not much today

--- Ass8.sql
+++ Ass8.sql
@@ -1,6 +1,6 @@
-CREATE TRIGGER after_orders_insert AFTER INSERT
- ON Orders
- FOR EACH ROW
- BEGIN
+CREATE TRIGGER after_orders_insert
+AFTER
+INSERT
+  ON Orders FOR EACH ROW BEGIN
     
- END;
+END;
