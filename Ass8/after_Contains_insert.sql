efdd2ba40763b62aaf46396ca9fd5acaea8e5590
V4G4X <varun.gawande@gmail.com>
2019-09-11 15:56:15 +0530

    Do DBMS from a good source.... Please... Please. PLEASE!

--- Ass8/after_Contains_insert.sql
+++ Ass8/after_Contains_insert.sql
@@ -6,23 +6,20 @@ FROM
 --   `Contains`,
   --   `Orders`,
   --   `Item`
----------------------------------
-SHOW CREATE TABLE `Contains`;
+  ---------------------------------
+  SHOW CREATE TABLE `Contains`;
 ----------------------------
-
 WHERE
   `Orders`.`OID` = `Contains`.`OID`
   AND `Contains`.`item_ID` = `Item`.`item_ID`;
 SHOW TRIGGERS;
 -------------------
-DROP TRIGGER IF EXISTS `after_Contains_insert`;
+  DROP TRIGGER IF EXISTS `after_Contains_insert`;
 CREATE TRIGGER `after_Contains_insert`
-AFTER
-INSERT
-  ON `Contains` FOR EACH ROW BEGIN --Declare temp Variable
-  
+AFTER INSERT ON `Contains` FOR EACH ROW BEGIN 
+--Declare temp Variable
   DECLARE `cost` INT;
-  DECLARE `temp` INT;
+DECLARE `temp` INT;
 DECLARE `quant` INT;
 DECLARE `item-id` INT;
 -- set ttl_amt = ()
--- Ass8/after_orders_insert_timestamp.sql
+++ Ass8/after_orders_insert_timestamp.sql
@@ -2,25 +2,25 @@ USE OrderManagementSystem;
 DROP TRIGGER IF EXISTS `after_orders_insert_timestamp`;
 DELIMITER $$
 CREATE TRIGGER `after_orders_insert_timestamp`
-AFTER
-INSERT
-  ON `Orders` FOR EACH ROW BEGIN
-SELECT
-  @var;
-SET
-  @var = (
-    SELECT
-      CURRENT_TIMESTAMP
-  );
-  SET Orders.timestmp = @var;
-END;
-$$ DELIMITER;
-
-show create table Orders;
-SHOW TRIGGERS;
-
-ALTER TABLE `Orders` MODIFY `timestmp` TIMESTAMP NOT NULL AFTER `OID`;
-
-SELECT @var;
-SET @var = (SELECT CURRENT_TIMESTAMP);
-select @var;
+AFTER INSERT ON `Orders` FOR EACH ROW
+BEGIN
+  SET @IID = (SELECT `UID` FROM `User` WHERE ( `User`.`UID` = 8));
+  SELECT IF( @IID IS NULL, "Invalid UID","UID is Valid") AS "Message";
+  Call Test();
+END$$
+DELIMITER ; 
+-----------------------------------------------------------------------
+SET @IID = (SELECT `UID` FROM `User` WHERE ( `User`.`UID` = NEW.UID));
+SELECT IF( @IID IS NULL, "Invalid UID","UID is Valid") AS "Message";
+-----------------------------------------------------------------------
+Select @IID;
+SET @IID = (SELECT `UID` FROM `User` WHERE ( `User`.`UID` = 8));
+-----------------------------------------------------------------------
+DELIMITER $$
+CREATE PROCEDURE TEST() BEGIN
+END
+$$
+DELIMITER ;
+-----------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `Test`;
+  
